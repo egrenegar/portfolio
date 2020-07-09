fa129a88c7e881eebb7fbf22e6dbcde867860ac3
Emily Renegar <egrenegar@gmail.com>
2020-07-08 23:35:34 -0400

    first build

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -25,7 +25,7 @@ function Navigation() {
                 </div>
                 <div className="navbar-item">
                     <a>
-                        Contact
+                        Test
                     </a>
                 </div>
             </div>
