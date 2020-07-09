1ac52eab9bafbbd5a38ad5a59de23058376d45e0
Emily Renegar <egrenegar@gmail.com>
2020-07-09 09:05:41 -0400

    imported logo, removed/added classes in navbar brand

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import styles from './navbar.css';
+import logo from '../../img/PORTFOLIO_LOGO.png';
 
 import { Navbar } from 'react-bulma-components';
 
 function Navigation() {
     return (
         <Navbar style={styles.navbar}>
-            <div className="navbar-brand">
-                <a className="navbar-item" href="https://github.com/egrenegar/egrenegar.github.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" alt='brand' width="112" height="28" />
+            <div className='logo-div'>
+                <a href="https://github.com/egrenegar/egrenegar.github.io">
+                    <img src={logo} alt='brand' width='200'/>
                 </a>
             </div>
             <div className="navbar-end">
