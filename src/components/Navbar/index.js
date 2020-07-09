572230e087aeb7107eebfc5fe0bb9108598ccb48
Emily Renegar <egrenegar@gmail.com>
2020-07-08 23:10:34 -0400

    added Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import styles from './navbar.css';
+
+import { Navbar } from 'react-bulma-components';
+
+function Navigation() {
+    return (
+        <Navbar style={styles.navbar}>
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://github.com/egrenegar/egrenegar.github.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt='brand' width="112" height="28" />
+                </a>
+            </div>
+            <div className="navbar-end">
+                <div className="navbar-item">
+                    <a>
+                        About
+                    </a>
+                </div>
+                <div className="navbar-item">
+                    <a>
+                        Projects
+                    </a>
+                </div>
+                <div className="navbar-item">
+                    <a>
+                        Contact
+                    </a>
+                </div>
+            </div>
+        </Navbar>
+    );
+
+}
+
+export default Navigation;
