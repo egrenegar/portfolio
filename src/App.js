3194652ccd3b02e89cd9789de7bfeb768ff86050
Emily Renegar <egrenegar@gmail.com>
2020-07-10 11:14:25 -0400

    updated Contact route

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Route exact path="/" component={About} />
       <Route exact path="/about" component={About} />
       <Route exact path="/projects" component={Projects} />
-      <Route path="/contact" component={Contact} />
+      <Route exact path="/contact" component={Contact} />
     </div>
   </Router>
     
