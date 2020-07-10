fd6f0123875d550cf9afd37263e60ed3b1078ddc
Emily Renegar <egrenegar@gmail.com>
2020-07-09 22:58:30 -0400

    heading renders with inline styles

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Heading from '../Heading/index';
+
+const styles = {
+    heading: {
+        fontFamily: 'Josefin Sans',
+        fontWeight: 400,
+        fontSize: '3rem',
+        paddingTop: '6rem'
+    }
+}
 
 function About() {
     return (
-        <div>
-            <div className='wrapper'>
-                <div className="jumbotron jumbotron-fluid">
-                    <div className="container">
-                        <h1 className="display-4 text-center">Pupster</h1>
-                        <p className="lead text-center">They're the Good Boys and Girls.</p>
-                    </div>
-                </div>
-            </div>
-            <div className='px-5'>
-            <h2>Welcome to Pupster!</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            </div>
-         </div>
+        <Fragment>
+           <Heading>
+               <h2 style={styles.heading}>Thanks for being here!</h2>
+           </Heading>
+         </Fragment>
        
     )
 }
