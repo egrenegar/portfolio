e1e3ff2cc7e71be2400d9d5a5b91108c3f7d7ed0
Emily Renegar <egrenegar@gmail.com>
2020-07-09 22:09:10 -0400

    added routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Navbar from './components/Navbar/index'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div>
       <Navbar />
+      <Route exact path="/" component={About} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
     </div>
+  </Router>
+    
   );
 }
 
 export default App;
+
