f03aa0c1d9d441cc57b6f0bc496fc0ab9379dddc
Emily Renegar <egrenegar@gmail.com>
2020-07-10 11:16:55 -0400

    removed <Fragment/>

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Heading from '../Heading/index';
 import AboutBody from '../AboutBody/index';
 import { Container} from 'react-bulma-components';
@@ -15,14 +15,12 @@ const styles = {
 
 function About() {
     return (
-        <Fragment>
-            <Container>
-                <Heading>
-                    <h2 style={styles.heading}>Thanks for being here!</h2>
-                </Heading>
-                <AboutBody/>
-            </Container>
-        </Fragment>
+        <Container>
+            <Heading>
+                <h2 style={styles.heading}>Thanks for being here!</h2>
+            </Heading>
+            <AboutBody/>
+        </Container>
     )
 }
 
