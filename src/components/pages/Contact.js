2d4160227f182b04427845cd2cd4bf011f1c8bc3
Emily Renegar <egrenegar@gmail.com>
2020-07-09 23:00:28 -0400

    heading renders with inline styles

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Heading from '../Heading/index';
+
+const styles = {
+    heading: {
+        fontFamily: 'Josefin Sans',
+        fontWeight: 400,
+        fontSize: '3rem',
+        paddingTop: '6rem'
+    }
+}
 
 function About() {
     return (
-        <div>
-           <h2>You can find me here:</h2>
-           <p>egrenegar@gmail.com</p>
-         </div>
+        <Fragment>
+           <Heading>
+               <h2 style={styles.heading}>You can find me here:</h2>
+           </Heading>
+         </Fragment>
        
     )
 }
