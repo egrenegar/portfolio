86d57414dde6a5538b25354da4182ba8f57d0bd1
Emily Renegar <egrenegar@gmail.com>
2020-07-10 10:57:04 -0400

    Updates

---
+++ precache-manifest.f7624df729fae58511196f07baeafcad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8640fc3296ba2c83de93a4e611e497",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "58cb972add38b9ce7be7",
+    "url": "/portfolio/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "50eadafff757d4c8531c",
+    "url": "/portfolio/static/css/main.b0daf5a4.chunk.css"
+  },
+  {
+    "revision": "58cb972add38b9ce7be7",
+    "url": "/portfolio/static/js/2.62733cb3.chunk.js"
+  },
+  {
+    "revision": "6c5833db93cd7c0f813219a8bc75480b",
+    "url": "/portfolio/static/js/2.62733cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50eadafff757d4c8531c",
+    "url": "/portfolio/static/js/main.7c43f7da.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e053a5be66e2a13d00df717e7023e7cf",
+    "url": "/portfolio/static/media/PORTFOLIO_LOGO.e053a5be.png"
+  },
+  {
+    "revision": "aaa44df63e38fcd533ce421e561fd1c7",
+    "url": "/portfolio/static/media/PortfolioImage.aaa44df6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d4dc4dafe9bd92cde3b4ea4063f6c77b.js"
+  "/portfolio/precache-manifest.f7624df729fae58511196f07baeafcad.js"
 );
 
 self.addEventListener('message', (event) => {
