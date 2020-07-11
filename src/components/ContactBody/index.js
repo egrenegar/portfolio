813494f970bcf062508295b8cde6234a2bb0a567
Emily Renegar <egrenegar@gmail.com>
2020-07-11 18:20:50 -0400

    adding ContactBody component and stylesheet

---
+++ src/components/ContactBody/index.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+import { Columns } from 'react-bulma-components';
+import './ContactBody.css';
+
+export default function ContactBody() {
+    return(
+        <Fragment>
+            <Columns>
+                <Columns.Column className='has-text-centered text'>
+                    <p>egrenegar@gmail.com</p>
+                </Columns.Column>
+            </Columns>
+            <Columns>
+                <Columns.Column className='has-text-centered text'>
+                    <span><a href='https://github.com/egrenegar' rel='noopener noreferrer' target='_blank' className='icon'><i className="fab fa-github fa-2x"></i></a></span>
+                </Columns.Column>
+            </Columns>
+            <Columns>
+                <Columns.Column className='has-text-centered text'>
+                    <span><a href='https://www.linkedin.com/in/emily-renegar' rel='noopener noreferrer' target='_blank' className='icon'><i className="fab fa-linkedin-in fa-2x"></i></a></span>
+                </Columns.Column>
+            </Columns>
+        </Fragment>
+    );
+    
+};
