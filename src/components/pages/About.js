0897c9549ef71e2bb8cf3e16bb01a5af8d883d30
Emily Renegar <egrenegar@gmail.com>
2020-07-11 17:22:51 -0400

    removed inline styles

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,21 +3,11 @@ import Heading from '../Heading/index';
 import AboutBody from '../AboutBody/index';
 import { Container} from 'react-bulma-components';
 
-
-const styles = {
-    heading: {
-        fontFamily: 'Josefin Sans',
-        fontWeight: 400,
-        fontSize: '3rem',
-        paddingTop: '6rem'
-    }
-}
-
 function About() {
     return (
         <Container>
             <Heading>
-                <h2 style={styles.heading}>Thanks for being here!</h2>
+                <h2>Thanks for being here!</h2>
             </Heading>
             <AboutBody/>
         </Container>
