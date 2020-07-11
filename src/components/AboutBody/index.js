623cf6ed6bcd6c93c6348a9f34d0fe2ef089074a
Emily Renegar <egrenegar@gmail.com>
2020-07-11 17:00:37 -0400

    added flex-item classNames and rewrote Bulma desktop Col size

--- src/components/AboutBody/index.js
+++ src/components/AboutBody/index.js
@@ -6,11 +6,20 @@ import './AboutBody.css';
 export default function AboutBody() {
     return(
         <Fragment>
-            <Columns className='is-centered'>
-                <Columns.Column size={3}>
+            <Columns className='is-centered reverse-columns'>
+                <Columns.Column
+                    desktop={{
+                        size: 3,
+                    }}
+                    className='flex-item-1'>
                     <img className='image' alt='Emily Renegar' src={image} />
                 </Columns.Column>
-                <Columns.Column size={4}>
+                <Columns.Column
+                    desktop={{
+                        size: 4,
+                    }}
+                    className='flex-item-2'
+                >
                     <p className='text'>
                         My name is Emily. I'm a Front-End Developer whose interest in coding was sparked as a pre-teen spending too much time customizing themes on MySpace.
                     </p>
@@ -29,7 +38,6 @@ export default function AboutBody() {
                         <span><a href='https://github.com/egrenegar' rel='noopener noreferrer' target='_blank'><i class="fab fa-github"></i></a></span>
                         <span><a href='https://www.linkedin.com/in/emily-renegar' rel='noopener noreferrer' target='_blank'><i class="fab fa-linkedin-in"></i></a></span>
                     </p>
-                    
                 </Columns.Column>
             </Columns>
         </Fragment>
