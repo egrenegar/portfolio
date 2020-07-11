9021cf070bf69d693593a0c6e1a11975780d9560
Emily Renegar <egrenegar@gmail.com>
2020-07-11 17:21:56 -0400

    added className for styling

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Columns } from 'react-bulma-components';
+import './heading.css';
 
 export default function Heading(props) {
     return(
-        <Columns className="has-text-centered">
-            <Columns.Column>
+        <Columns className='has-text-centered'>
+            <Columns.Column className='portfolio-heading'>
                 {props.children}
             </Columns.Column>
         </Columns>
