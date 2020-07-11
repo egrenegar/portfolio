ad2e29268a3e05ad5fe41cf0b8ddb7687ba7f484
Emily Renegar <egrenegar@gmail.com>
2020-07-11 17:23:21 -0400

    removed inline styles

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,23 +1,13 @@
 import React, {Fragment} from 'react';
 import Heading from '../Heading/index';
 
-const styles = {
-    heading: {
-        fontFamily: 'Josefin Sans',
-        fontWeight: 400,
-        fontSize: '3rem',
-        paddingTop: '6rem'
-    }
-}
-
 function About() {
     return (
         <Fragment>
            <Heading>
-               <h2 style={styles.heading}>You can find me here:</h2>
+               <h2>You can find me here:</h2>
            </Heading>
-         </Fragment>
-       
+        </Fragment>
     )
 }
 
