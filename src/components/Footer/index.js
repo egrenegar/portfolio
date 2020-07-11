fe290189a08e953776f48167915d593f3cbc687f
Emily Renegar <egrenegar@gmail.com>
2020-07-11 18:30:53 -0400

    creating Footer component and stylesheet

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Columns } from 'react-bulma-components';
+import './footer.css';
+
+export default function Footer() {
+    return(
+        <Columns className='has-text-centered'>
+            <Columns.Column>
+                <div className='my-footer'>
+                    Copyright 2020 Emily Renegar
+                </div>
+            </Columns.Column>
+        </Columns>
+    );
+};
