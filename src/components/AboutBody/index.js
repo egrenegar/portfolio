b4166071d8ee8af6984941a25221fa1b0402b43d
Emily Renegar <egrenegar@gmail.com>
2020-07-10 22:54:46 -0400

    added github and linkedin icons, adjusted font/image size

--- src/components/AboutBody/index.js
+++ src/components/AboutBody/index.js
@@ -7,10 +7,10 @@ export default function AboutBody() {
     return(
         <Fragment>
             <Columns className='is-centered'>
-                <Columns.Column size={2}>
+                <Columns.Column size={3}>
                     <img className='image' alt='Emily Renegar' src={image} />
                 </Columns.Column>
-                <Columns.Column size={3}>
+                <Columns.Column size={4}>
                     <p className='text'>
                         My name is Emily. I'm a Front-End Developer whose interest in coding was sparked as a pre-teen spending too much time customizing themes on MySpace.
                     </p>
@@ -26,7 +26,10 @@ export default function AboutBody() {
                 <Columns.Column>
                     <p className='contact'>
                         <span>Contact:</span> egrenegar@gmail.com
+                        <span><a href='https://github.com/egrenegar' rel='noopener noreferrer' target='_blank'><i class="fab fa-github"></i></a></span>
+                        <span><a href='https://www.linkedin.com/in/emily-renegar' rel='noopener noreferrer' target='_blank'><i class="fab fa-linkedin-in"></i></a></span>
                     </p>
+                    
                 </Columns.Column>
             </Columns>
         </Fragment>
