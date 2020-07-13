f2e9d49c01519d8b3b00fb5fdc0ad10eb5e74f6f
Emily Renegar <egrenegar@gmail.com>
2020-07-12 23:00:26 -0400

    updated relative paths

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from "./components/pages/About";
-import Projects from "./components/pages/Projects";
-import Contact from "./components/pages/Contact";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
