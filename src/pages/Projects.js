8b245041355da149c48662303eed7957e11fb692
Emily Renegar <egrenegar@gmail.com>
2020-07-16 22:41:47 -0400

    passing in necessary props for overlay text

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from 'react-bulma-components';
 import ProjectFull from '../components/ProjectFull/index';
-import ProjectHalf from '../components/ProjectHalf/index';
 import Footer from '../components/Footer/index';
+import Overlay from '../components/Overlay/index';
 import AFAF from '../img/AFAF_mockup_compressed.png';
 import Intown from '../img/INTown_mockup_compressed.png';
 import Innovation from '../img/InnovationAuto_mockup.png';
@@ -13,16 +13,36 @@ function Projects() {
             <ProjectFull
                 src={AFAF}
                 alt='Asking For a Friend'
-            />
+                id='one'
+            >
+                <Overlay
+                    className='overlay'
+                    name='Asking For a Friend'
+                    technologies={['Handlebars.js', 'Bulma CSS', 'jQuery', 'MySQL', 'Express']}
+                />
+            </ProjectFull>
             <ProjectFull
                 src={Innovation}
                 alt='Innovation Auto Hail Repair'
-            />
+                id='two'
+            >
+                <Overlay
+                    className='overlay'
+                    name='Innovation Auto Repair'
+                    technologies={['React','Next.js', 'Pure CSS & Flexbox', 'JavaScript']}
+                />
+            </ProjectFull>
             <ProjectFull
                 src={Intown}
                 alt='INTown'
-            />
-            
+                id='three'
+            >
+                <Overlay
+                    className='overlay'
+                    name='INTown'
+                    technologies={['HTML', 'Foundation CSS', 'JavaScript', 'Google Maps API']}
+                />
+            </ProjectFull>
             <Footer/>
         </Container>
     )
