1ce4482d5e945d8505a916a4cc1e3e9d1fddb32a
Emily Renegar <egrenegar@gmail.com>
2020-07-17 16:41:44 -0400

    changed className to contact-text

--- src/components/ContactBody/index.js
+++ src/components/ContactBody/index.js
@@ -6,17 +6,17 @@ export default function ContactBody() {
     return(
         <Fragment>
             <Columns>
-                <Columns.Column className='has-text-centered text'>
+                <Columns.Column className='has-text-centered contact-text'>
                     <p>egrenegar@gmail.com</p>
                 </Columns.Column>
             </Columns>
             <Columns>
-                <Columns.Column className='has-text-centered text'>
+                <Columns.Column className='has-text-centered'>
                     <span><a href='https://github.com/egrenegar' rel='noopener noreferrer' target='_blank' className='icon'><i className="fab fa-github fa-2x"></i></a></span>
                 </Columns.Column>
             </Columns>
             <Columns>
-                <Columns.Column className='has-text-centered text'>
+                <Columns.Column className='has-text-centered'>
                     <span><a href='https://www.linkedin.com/in/emily-renegar' rel='noopener noreferrer' target='_blank' className='icon'><i className="fab fa-linkedin-in fa-2x"></i></a></span>
                 </Columns.Column>
             </Columns>
