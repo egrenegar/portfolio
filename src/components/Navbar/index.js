5bf0045a8877ebeb582e8cb693ef524d03fc5cc0
Emily Renegar <egrenegar@gmail.com>
2020-07-18 16:18:07 -0400

    added hamburger menu <Link> and function to handle nav 'is-active' class

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,31 +5,55 @@ import logo from '../../img/PORTFOLIO_LOGO.png';
 
 import { Navbar } from 'react-bulma-components';
 
+const handleMenu = () => {
+    const burger = document.querySelector('.navbar-burger');
+    const menu = document.querySelector('.navbar-menu');
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+}
+
 export default function Navigation() {
+
     return (
         <Navbar style={styles.navbar}>
-            <div className='logo-div'>
+            <div className='navbar-brand'>
                 <Link to='/'>
-                    <img src={logo} alt='brand' width='200'/>
+                    <img src={logo} alt='brand' width='200' />
+                </Link>
+
+                <Link
+                    role='button'
+                    className='navbar-burger burger'
+                    aria-label='menu'
+                    aria-expanded='false'
+                    onClick={handleMenu}
+                >
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
                 </Link>
             </div>
-            <div className='navbar-end'>
-                <div className='navbar-item'>
-                    <Link to='/about'>
-                        About
-                    </Link>
-                </div>
-                <div className='navbar-item'>
-                    <Link to='/projects'>
-                        Projects
-                    </Link>
-                </div>
-                <div className='navbar-item'>
-                    <Link to='/contact'>
-                        Contact
-                    </Link>
+           
+            <div className='navbar-menu'>
+                <div className='navbar-end'>
+                    <div className='navbar-item'>
+                        <Link to='/about'>
+                            About
+                        </Link>
+                    </div>
+                    <div className='navbar-item'>
+                        <Link to='/projects'>
+                            Projects
+                        </Link>
+                    </div>
+                    <div className='navbar-item'>
+                        <Link to='/contact'>
+                            Contact
+                        </Link>
+                    </div>
                 </div>
             </div>
+            
         </Navbar>
     );
 
