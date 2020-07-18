8cd3b266940bcfea9ee14f37453950915b963ad8
Emily Renegar <egrenegar@gmail.com>
2020-07-18 19:48:26 -0400

    passing in github and deployed links in AFAF

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,10 +16,11 @@ function Projects() {
                 id='one'
             >
                 <Overlay
-                    className='overlay'
                     name='Asking for a Friend'
                     technologies={['Handlebars.js', 'Bulma CSS', 'jQuery', 'MySQL', 'Express']}
                     description='Asking for a Friend is a question and answer website that allows users to post questions and others to respond.'
+                    github='https://github.com/egrenegar/Asking_for_a_friend'
+                    deployed='https://askingforafriend.herokuapp.com/'
                 />
             </ProjectFull>
             <ProjectFull
@@ -28,7 +29,6 @@ function Projects() {
                 id='two'
             >
                 <Overlay
-                    className='overlay'
                     name='Innovation Auto Repair'
                     technologies={['React','Next.js', 'Pure CSS & Flexbox', 'JavaScript']}
                     description='This full-stack website was built for a small auto body repair shop based in Texas. Its main function is to allow users to schedule free estimates, and for employees to manage appointments.'
@@ -40,7 +40,6 @@ function Projects() {
                 id='three'
             >
                 <Overlay
-                    className='overlay'
                     name='INTown'
                     technologies={['HTML', 'Foundation CSS', 'JavaScript', 'Google Maps API']}
                     description='INTown unlocks the discovery of events, outdoor spaces, popular places, and restaurants in Charlotte, NC neighborhoods.'
