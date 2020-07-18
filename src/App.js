1bd2f1e106a9488fc5e96853af68e58a45dc7e30
Emily Renegar <egrenegar@gmail.com>
2020-07-18 19:36:27 -0400

    added <Switch> and added dynamic Projects route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
   return (
     <Router>
     <div>
       <Navbar />
-      <Route exact path="/" component={About} />
-      <Route exact path="/portfolio" component={About} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/projects" component={Projects} />
-      <Route exact path="/contact" component={Contact} />
+      <Switch>
+        <Route exact path='/' component={About} />
+        <Route exact path='/portfolio' component={About} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/projects' component={Projects} />
+        <Route exact path='/contact' component={Contact} />
+        <Route path='/projects/:id' component={ProjectDetails} />
+      </Switch>
     </div>
   </Router>
     
