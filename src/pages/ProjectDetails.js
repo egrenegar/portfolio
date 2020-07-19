f09000ea790a5297380402dc4b0becbaf8efed0e
Emily Renegar <egrenegar@gmail.com>
2020-07-19 18:21:40 -0400

    rendering page content inside page instead of separate component

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,8 +1,76 @@
-import React from 'react';
-import Details from '../components/Details/index';
+import React, { useState } from 'react';
+import { Columns, Heading } from 'react-bulma-components';
+import './projectdetails.css';
 
-export default function ProjectDetails() {
-    return(
-       <Details/>
+const details = require('../details.json');
+
+// const style = {
+//     details: {
+//         width: '75vw',
+//         margin: '2rem auto',
+//         textAlign: 'center',
+//         fontFamily: ['Josefin Sans', 'sans-serif'],
+//     },
+//     hr: {
+//         color: 'black',
+//         border: '1px solid rgb(241, 136, 194)',
+//         width: '25%',
+//         margin: '10px auto'
+//     }
+// }
+
+export default function ProjectDetails(props) {
+
+    const id = props.match.params.id - 1;
+
+    const [projectState] = useState({
+        details,
+        id,
+        name: details[id].name,
+        description: details[id].description,
+        technologies: details[id].technologies,
+        github: details[id].github,
+        deployed: details[id].deployed
+    });
+
+    return (
+        <Columns>
+            <Columns.Column
+                className='details-column'
+                mobile={{
+                    size: 10
+                }}
+                desktop={{
+                    size: 5,
+                }}
+            >
+                <div className='project-details'>
+                    <div className='details-heading'>
+                        <Heading className='details-name' size={3}>{projectState.name}</Heading>
+                        <p className='details-description'>{projectState.description}</p>
+                        <hr className='details-hr' />
+                    </div>
+                    <div className='technologies'>
+                        <p>Built With:</p>
+                        <br />
+                        <ul>
+                            {projectState.technologies.map(technology => (
+                                <li className='technology'>
+                                    {technology}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <Columns className='is-centered'>
+                    <Columns.Column desktop={{size: 3}} className='details-links has-text-centered'>
+                        <a href={projectState.github} target='_blank' rel='noopener noreferrer'>GitHub &gt;</a>
+                    </Columns.Column>
+                    <Columns.Column desktop={{size: 3}} className='details-links has-text-centered'>
+                        <a href={projectState.deployed} target='_blank' rel='noopener noreferrer'>Deployed App &gt;</a>
+                    </Columns.Column>
+                </Columns>
+            </Columns.Column>
+        </Columns>
     );
 };
