da087f845e4fa9904fa114b685ac3bffd36bd636
Emily Renegar <egrenegar@gmail.com>
2020-07-21 10:44:49 -0400

    adding missing github and deployed props for <Overlay/> component

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -34,6 +34,8 @@ function Projects() {
                     name='Innovation Auto Repair'
                     technologies={['React','Next.js', 'Pure CSS & Flexbox', 'JavaScript']}
                     description='This full-stack website was built for a small auto body repair shop based in Texas. Its main function is to allow users to schedule free estimates, and for employees to manage appointments.'
+                    github='https://github.com/egrenegar/Innovation-Auto-Repair'
+                    deployed='https://innovation-pdr.herokuapp.com/'
                 />
             </ProjectFull>
             <ProjectFull
@@ -46,6 +48,8 @@ function Projects() {
                     name='INTown'
                     technologies={['HTML', 'Foundation CSS', 'JavaScript', 'Google Maps API']}
                     description='INTown unlocks the discovery of events, outdoor spaces, popular places, and restaurants in Charlotte, NC neighborhoods.'
+                    github='https://github.com/egrenegar/INTown'
+                    deployed='https://graysondeese.github.io/INTown/'
                 />
             </ProjectFull>
             <Footer/>
